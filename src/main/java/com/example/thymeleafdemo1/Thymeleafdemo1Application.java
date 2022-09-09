44ad13bd2062853be4b7562b9f26f3526f7aa8b4
Prasun <prasunhowlader@yahoo.com>
2022-09-09 15:44:53 +0600

    New Thyme leaf project

---
+++ src/main/java/com/example/thymeleafdemo1/Thymeleafdemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thymeleafdemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Thymeleafdemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thymeleafdemo1/Thymeleafdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thymeleafdemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
